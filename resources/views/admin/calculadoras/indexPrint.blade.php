67ad23d65b3142d485012318482380879ab36d94
arielMsi <ariel.taipe2640@utc.edu.ec>
2022-12-19 21:54:44 -0500

    pdf

--- resources/views/admin/calculadoras/indexPrint.blade.php
+++ resources/views/admin/calculadoras/indexPrint.blade.php
@@ -3,10 +3,13 @@
 @section('title', 'Imprimir')
 
 @section('content_header')
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
+    <x-adminlte-info-box title="NOTAS ADICIONALES" text="
+    -Cotización no es válida para carga peligrosa o perecedera, ni para carga Bonded.
+    -Es válida para carga General, apilable y correctamente embalada. Casos EUR1, originales entregamos al SHIPPER.
+    -Cotización NO incluye el seguro de la mercadería. Favor enviarnos el valor CFR de la carga para proceder con la cotización respectiva.
+    -Muy importante notar que la Aduana Ecuatoriana se reserva el derecho de aplicar o no las partidas enviadas por los clientes/ agentes.
+
+    " icon="fas fa-lg fa-eye text-dark" theme="gradient-teal" />
 @stop
 
 @section('content')
@@ -41,5 +44,3 @@
 
     </div><br>
 @stop
-
-
