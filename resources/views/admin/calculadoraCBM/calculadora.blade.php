923b8e5483a7f81468bcf4fea879f786d7ca2b19
msiAriel <ariel.taipe2640@utc.edu.ec>
2023-01-19 21:17:09 -0500

    fgss

--- resources/views/admin/calculadoraCBM/calculadora.blade.php
+++ resources/views/admin/calculadoraCBM/calculadora.blade.php
@@ -3,184 +3,143 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Calculadora de Metros Cubicos</h1>
-                {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> --}}
-                <button class="btn add-btn btn-info">+</button>
-                <button class="btn rest-btn btn-info">-</button>
+                <button type="button" onclick="agregarProducto()" class="btn btn-primary" >Agregar</button><br>
             </div>
             <div class="modal-body">
 
                 <div class="row">
-                    <div class="col-md-9">
-                        <form id="form" class="calculadora">
-                            <div class="input-group">
-
-                                <div class="form-floating ">
-                                    <input type="number" min="0" class="form-control" id="cantidad"
-                                        name="cantidad" placeholder="name@example.com">
-                                    <label for="floatingInput">Cantidad</label>
-                                </div>
-                                <div class="form-floating ">
-                                    <input type="number" min="0" class="form-control" id="longitud"
-                                        name="longitud" placeholder="name@example.com">
-                                    <label for="floatingInput">Longitud</label>
-                                </div>
-                                <div class="form-floating ">
-                                    <input type="number" min="0" class="form-control" id="anchura"
-                                        name="anchura" placeholder="name@example.com">
-                                    <label for="floatingInput">Ancho</label>
-                                </div>
-                                <div class="form-floating ">
-                                    <input type="number" min="0" class="form-control" id="altura"
-                                        name="altura" placeholder="name@example.com">
-                                    <label for="floatingInput">Altura</label>
-                                </div>
-                            </div><br>
-
-                        </form>
-                    </div>
-                    <div class="col-md-3">
-
-                        <div class="form-floating ">
-                            <input type="text" class="form-control" id="total" disabled style="color: red">
-                            <label for="floatingInput">Volumen</label>
-                        </div>
-
-
+                    <div class="col-md-12">
+                        <from>
+                   
+                            <!-- Trigger the modal with a button -->
+                            
+                            <input type="hidden" id="ListaPro" name="ListaPro" value="" required /><br>
+                            <table id="TablaPro" class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Cartones</th>
+                                        <th>Longitud</th>
+                                        <th>Anchura</th>
+                                        <th>Altura</th>
+                                        <th>Volumen</th>
+                                        <th>Acción</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="ProSelected">
+                                    <!--Ingreso un id al tbody-->
+                                    <tr>
+                
+                                    </tr>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <td colspan="2">Volumen Total</td>
+                                        
+                                        <td>&nbsp;</td>
+                                        <td><span id="total">0</span> <input class="form-control" type="hidden" id="total_final" name="total_final"  value="0" readonly /></td>
+                                        <td>&nbsp;</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                            <!--Agregue un boton en caso de desear enviar los productos para ser procesados-->
+                           
+                        </from>
                     </div>
                 </div>
-                <div class="newData"></div>
 
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="submit" form="form" class="btn btn-primary">Calcular</button>
-            </div>
+            
         </div>
     </div>
 </div>
 <script>
-    $(function() {
-        var i = 0;
-        var j = 0;
-        var m = 0;
-        $('.add-btn').click(function(e) {
-            e.preventDefault();
-            j++;
-            i++;
-            m++;
-            $('.newData').append('<div class="row" id="newRow' + i + '">' +
-                '<div class="col-md-9">' +
-                '<form id="forms" class="calculadora">' +
-                '<div class="input-group">' +
-                '<div class="form-floating ">' +
-                '<input type="number" min="0"  class="form-control"  name="cantidad' + i +
-                '" id="cantidad' + i + '" placeholder="name@example.com">' +
-                '<label for="floatingInput">Cantidad</label>' +
-                '</div>' +
-                '<div class="form-floating ">' +
-                '<input type="number" min="0"  class="form-control"  name="longitud' + i +
-                '" id="longitud' + i + '" placeholder="name@example.com">' +
-                '<label for="floatingInput">Longitud</label>' +
-                '</div>' +
-                '<div class="form-floating ">' +
-                '<input type="number" min="0"  class="form-control" id="anchura' + i +
-                '" name="anchura' + i + '" placeholder="name@example.com">' +
-                '<label for="floatingInput">Ancho</label>' +
-                '</div>' +
-                '<div class="form-floating ">' +
-                '<input type="number" min="0" class="form-control" id="altura' + i +
-                '" name="altura' + i + '" placeholder="name@example.com">' +
-                '<label for="floatingInput">Altura</label>' +
-                '</div>' +
-                '</div><br>' +
-                '</form>' +
-                '</div>' +
-                '<div class="col-md-3">' +
-                '<div class="form-floating " >' +
-                '<input type="text" class="form-control" id="total' + i +
-                '"  disabled style="color: red">' +
-                '<label for="floatingInput">Volumen Total</label>' +
-                '</div>' +
-                '</div>'
-
-            );
-
-            $('.calculadora').submit(function(e) {
-                e.preventDefault();
-                var k = 1;
-
-                var contador = 0;
-                for (let step = 0; step < j; step++) {
-                    let cantidad = $("#cantidad" + k).val();
-                    let longitud = $("#longitud" + k).val();
-                    let anchura = $("#anchura" + k).val();
-                    let altura = $("#altura" + k).val();
-
-
-
-                    if (cantidad <= 0 || longitud <= 0 || anchura <= 0 || altura <= 0) {
-
-                        document.getElementById('total' + k).value = "Revisar los campos";
-                    } else {
-                        let result = (longitud * anchura * altura) / 1000000;
-                        let total = result * cantidad;
-                        contador = contador + total;
-                        let valor = document.getElementById('total').value;
-                        document.getElementById('total' + k).value = (parseFloat(total))
-                            .toFixed(2);
-                        document.getElementById('volumen').value = (parseFloat(contador) +
-                            parseFloat(valor)).toFixed(2);
-                    }
-                    k++
-
-                }
-
-                // ----------------------------
+    function RefrescaProducto() {
+            var ip = [];
+            var i = 0;
+            $('#guardar').attr('disabled', 'disabled'); //Deshabilito el Boton Guardar
+            $('.iProduct').each(function(index, element) {
+                i++;
+                ip.push({
+                    id_pro: $(this).val()
+                });
             });
-
-        });
-
-
-        $('.rest-btn').click(function(e) {
-            e.preventDefault();
-            
-            var max = 0;
-            for (var i = j; i >= 1; i--) {
-                if (i > max) {
-                    max = i;
-                }
-                
+            // Si la lista de Productos no es vacia Habilito el Boton Guardar
+            if (i > 0) {
+                $('#guardar').removeAttr('disabled', 'disabled');
             }
-
-            console.log(max);
-            $('#newRow' + max + '').remove();
-            j--;
-        });
-
-    });
-
-    $('.calculadora').submit(function(e) {
-        e.preventDefault();
-
-        // -----------------------------
-        let cantidad = $("#cantidad").val();
-        let longitud = $("#longitud").val();
-        let anchura = $("#anchura").val();
-        let altura = $("#altura").val();
-
-
-        if (cantidad <= 0 || longitud <= 0 || anchura <= 0 || altura <= 0) {
-
-            document.getElementById('total').value = "Revisar los campos";
-        } else {
-            let result = (longitud * anchura * altura) / 1000000;
-            let total = result * cantidad;
-            document.getElementById('total').value = (parseFloat(total)).toFixed(2);
-            document.getElementById('volumen').value = (parseFloat(total)).toFixed(2);
-
+            var ipt = JSON.stringify(ip); //Convierto la Lista de Productos a un JSON para procesarlo en tu controlador
+            $('#ListaPro').val(encodeURIComponent(ipt));
         }
 
+        function agregarProducto() {
 
-        // ----------------------------
-    });
+            var sel = '.'; //Capturo el Value del Producto
+            var text = '.'; //Capturo el Nombre del Producto- Texto dentro del Select
+
+
+            var sptext = text.split();
+
+            var newtr = '<tr class="item"  data-id="'+sel+'">';
+            newtr = newtr + '<td class="iProduct" >' + sel + '</td>';
+            newtr = newtr + '<td><input class="form-control" type="text" id="cartones[]" name="lista[]" onChange="Calcular(this);" value="1" /></td><td><input class="form-control" type="text" id="cantidad[]" name="lista[]" onChange="Calcular(this);" value="1" /></td><td><input class="form-control" type="text" id="nose[]" name="lista[]" onChange="Calcular(this);" value="1" /></td><td><input class="form-control" type="text" id="precunit[]" name="lista[]" onChange="Calcular(this);" value="0"/></td><td><input class="form-control" type="text" id="totalitem[]" name="lista[]" readonly /></td>';
+            newtr = newtr + '<td><button type="button" class="btn btn-danger btn-xs remove-item" ><i class="fa fa-times"></i></button></td></tr>';
+
+            $('#ProSelected').append(newtr); //Agrego el Producto al tbody de la Tabla con el id=ProSelected
+
+            RefrescaProducto(); //Refresco Productos
+
+            $('.remove-item').off().click(function(e) {
+                var total = document.getElementById("total");
+                total.innerHTML = parseFloat(total.innerHTML) - parseFloat(this.parentNode.parentNode.childNodes[5].childNodes[0].value);
+                $(this).parent('td').parent('tr').remove(); //En accion elimino el Producto de la Tabla
+                if ($('#ProSelected tr.item').length == 0)
+                    $('#ProSelected .no-item').slideDown(300);
+                RefrescaProducto();
+                
+                Calcular(e.target);
+            });
+            $('.iProduct').off().change(function(e) {
+                RefrescaProducto();
+            
+            });
+        }
+
+        
+        function Calcular(ele) {
+            var cantidad = 0, precunit = 0, totalitem = 0, nose=0, vol=0, cartones=0 ;
+            var tr = ele.parentNode.parentNode;
+            var nodes = tr.childNodes;
+        
+            for (var x = 0; x<nodes.length;x++) {
+                if (nodes[x].firstChild.id == 'cartones[]') {
+                    cartones = parseFloat(nodes[x].firstChild.value,10);
+                }
+                
+                if (nodes[x].firstChild.id == 'cantidad[]') {
+                    cantidad = parseFloat(nodes[x].firstChild.value,10);
+                }
+                if (nodes[x].firstChild.id == 'nose[]') {
+                    nose = parseFloat(nodes[x].firstChild.value,10);
+                }
+                if (nodes[x].firstChild.id == 'precunit[]') {
+                    precunit = parseFloat(nodes[x].firstChild.value,10);
+                }
+                if (nodes[x].firstChild.id == 'totalitem[]') {
+                    anterior = nodes[x].firstChild.value;
+                    vol = (parseFloat(((precunit*cantidad*nose)/1000000),10)).toFixed(2);;
+                    totalitem = vol*cartones
+                    nodes[x].firstChild.value = totalitem;
+                }
+            }
+            // Resultado final de cada fila ERROR, al editar o eliminar una fila
+            var total = document.getElementById("total");
+            if (total.innerHTML == 'NaN') {
+                total.innerHTML = 0;
+                // 
+            }
+            total.innerHTML = (parseFloat(total.innerHTML)+totalitem -anterior).toFixed(2); ;    
+        }
+
+        
 </script>
--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     
     {{-- <link rel="stylesheet" href="{{ asset('css/reportes.css')}}" /> --}}
-    <link rel="stylesheet" href="../resources/css/invoices.css">
+    <link rel="stylesheet" href="../css/invoices.css">
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
